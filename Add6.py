34c09c11049ff8d04bdc287ab016678622804529
chaiteju <47323164+chaiteju@users.noreply.github.com>
2019-02-13 23:49:49 +0530

    Create Add6.py

---
+++ Add6.py
@@ -0,0 +1,38 @@
+from fractions import Fraction
+def add(t1,t2):
+    return Fraction(t1[0],t1[1])+Fraction(t2[0],t2[1])
+
+
+
+    
+         
+         
+     
+         
+         
+         
+         
+         
+
+
+   
+
+
+   
+    
+    
+    
+
+
+
+    
+    
+    
+
+
+
+    
+    
+    
+
+
